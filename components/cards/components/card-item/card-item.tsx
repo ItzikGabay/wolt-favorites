8ea6078c82198c678e94c9750a319eeed84b80f1
Itzikgabay <itzikdevio@gmail.com>
2022-09-03 02:12:46 +0300

    Added route for getting restauratns

--- components/cards/components/card-item/card-item.tsx
+++ components/cards/components/card-item/card-item.tsx
@@ -4,9 +4,11 @@ import {FunctionComponent} from "react";
 interface ICardProps {
     price: number;
     time: string;
+    showPrice?: boolean;
+    showTime?: boolean;
 }
 
-const CardItem: FunctionComponent<ICardProps> = ({price, time}) => {
+const CardItem: FunctionComponent<ICardProps> = ({price, time, showPrice= true, showTime = true}) => {
     return <div className={styles.container}>
         <div className={styles.rest_image} style={{backgroundImage: 'url("https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")'}}></div>
         <div className={styles.content}>
@@ -14,9 +16,9 @@ const CardItem: FunctionComponent<ICardProps> = ({price, time}) => {
             <div className={styles.subtitle}>works</div>
         </div>
         <div className={styles.footer}>
-            <div>₪{15}</div>
+            {showPrice && <div>₪{15}</div>}
             <span>&#183;</span>
-            <div>{'30-40'} min</div>
+            {showTime && <div>{'30-40'} min</div>}
         </div>
     </div>
 }
---
+++ pages/api/restaurants/index.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import axios, {AxiosResponse} from 'axios';
+
+const defaultRestaurants: string[] = ['vitrina'];
+
+const getRestaurant = async (name: string) => {
+    const baseURL: string = 'https://restaurant-api.wolt.com/v3/venues/slug';
+    const response: AxiosResponse = await axios.get(`${baseURL}/${name}`);
+    return response.data;
+}
+
+const getRestaurants: object[any] = async (ids) => {
+    const items: object[] = [];
+    for(let idx in ids) {
+        const name: string = ids[idx];
+        const item: object = await getRestaurant(name);
+        items.push(item);
+    }
+    return items;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if(req.body.ids && req.method === 'POST') {
+        const { ids }: string[] = req.body;
+
+        if(!Array.isArray(ids) || !ids.length) {
+            return res.status(200)
+                .json({
+                    status: "error",
+                    message: "'ids' value is not type of array or not having any items."
+                });
+        }
+        const response = await getRestaurants(ids);
+        return res.status(200).json({data: response, message: "success ids"});
+    }
+    const response = await getRestaurants(defaultRestaurants);
+    return res.status(200).json({data: response, message: "success"});
+}
