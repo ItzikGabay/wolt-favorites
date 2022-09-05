a46e80be32d0d587169d3973df2adc8d1771031e
Itzikgabay <itzikdevio@gmail.com>
2022-09-06 00:15:22 +0300

    Added order-with-me button

--- components/cards/components/card-item/card-item.tsx
+++ components/cards/components/card-item/card-item.tsx
@@ -1,6 +1,7 @@
 import styles from './card-item.module.scss';
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useEffect } from 'react';
 import GetOnline from '../../../get-online/get-online';
+import { useRouter } from 'next/router';
 
 interface ICardProps {
   price: number;
@@ -25,6 +26,15 @@ const CardItem: FunctionComponent<ICardProps> = ({
   showTime = true,
   image,
 }) => {
+  const router = useRouter();
+
+  const onClickOrderWith = async () => {
+    const link = `https://wa.me/${process.env.NEXT_PUBLIC_PHONE_NUMBER}?text=`;
+    const message = encodeURIComponent(`Pleaseeee order with me ${name}!`);
+    const url = `${link}${message}`;
+    await router.push(link);
+  };
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -39,8 +49,13 @@ const CardItem: FunctionComponent<ICardProps> = ({
         </div>
       </header>
       <main className={styles.content}>
-        <div className={styles.title}>{name}</div>
-        <div className={styles.subtitle}>{address}</div>
+        <div>
+          <div className={styles.title}>{name}</div>
+          <div className={styles.subtitle}>{address}</div>
+        </div>
+        <div className={styles.order_with_me} onClick={onClickOrderWith}>
+          Order with Itzik
+        </div>
       </main>
       <footer className={styles.footer}>
         {showPrice && <div>₪{15}</div>}
