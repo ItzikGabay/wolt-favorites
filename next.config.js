5f7b6958bc0fe586af985882a53e5e90e47dad86
Itzikgabay <itzikdevio@gmail.com>
2022-09-12 00:28:09 +0300

    Removed rewrites from nextjs config

--- next.config.js
+++ next.config.js
@@ -15,14 +15,6 @@ const nextConfig = {
     });
     return config;
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:3000/:path*'
-      }
-    ]
-  }
 };
 
 module.exports = nextConfig;
